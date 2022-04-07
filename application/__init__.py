db87dcdbe5cc6e8ea008679394d7b9729432ffd3
Sathurjan <sajisathurjan12@gmail.com>
2022-04-07 10:00:46 +0000

    complete issue #2

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ import uuid
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
-app.config['SECRET_ID'] = str(uuid.uuid4)
+app.config['SECRET_KEY'] = str(uuid.uuid4)
 
 db = SQLAlchemy(app)
 
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,11 @@
 from application import app
 from flask import render_template
+from application.forms import TaskForms
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add_tasks', methods=["GET", "POST"])
+def add_tasks():
+    form = TaskForms()
+    return render_template('add_tasks.html', form=form)
